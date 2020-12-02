3104b245873faba7df336aa6a7c701293ced524b
Antonio Adame <a.adame59@yahoo.com>
2020-12-02 09:18:17 -0800

    use env for login credentials

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 const fs = require("fs");
 const login = require("facebook-chat-api");
 const readline = require("readline");
+const custom_env = require("custom-env").env('staging');
+
+const login_email = process.env.login_email;
+const login_password = process.env.login_password;
+
+const UNICODE_THUMBDOWN = "\uD83D\uDC4E";
 
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const obj = {email: "dankfarrick404@gmail.com", password: "Aa20237657!"};
+const obj = {email: login_email, password: login_password};
 login(obj, (err, api) => {
-    if(err) {
-        switch (err.error) {
-            case 'login-approval':
-                console.log('Enter code > ');
-                rl.on('line', (line) => {
-                    err.continue(line);
-                    rl.close();
-                });
-                break;
-            default:
-                console.error(err);
-        }
-        return;
-    }
+    if(err) return console.error(err);
 
     // Logged in!
     api.listenMqtt((err, message) => {
-        api.sendMessage("This is the way", message.threadID);
+        if (message.senderID == 1364550358) {
+            api.setMessageReaction(UNICODE_THUMBDOWN, message.messageID, (err) => {
+                if (err) return console.error(err);
+            });
+        }
     });
 });
